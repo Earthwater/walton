f3ed7b7d9003b87598e84f8a5a95dc741db7454d
yngfng <yngfng@gmail.com>
2017-06-18 18:24:37 +0800

    完成 Crowdsale 业务逻辑修改

--- contracts/Crowdsale.sol
+++ contracts/Crowdsale.sol
@@ -1,8 +1,21 @@
 pragma solidity ^0.4.8;
 
-import './ERC20.sol';
+import './ERC20.sol'; // yangfeng: 可能不需要？
 import './SafeMath.sol';
 import './MultiSigWallet.sol';
+
+// 主要功能：
+// --------------------
+// 1，合约创建时，设置开始时间，结束时间，最高ETH目标，最低ETH目标；
+// 2，众筹进行时，投资者向合约地址发送 ETH，触发合约把发送者地址和 ETH 记录到 weiAmountOf，供投资者查询，以确定投资成功
+//    当接收最后一笔达到最高目标时，退回剩余 ETH 给投资者；
+// 3，众筹成功，手动执行 finalizeCrowdfunding()，把合约帐户 ETH 一次性转入 multiSigWallet
+// 4，众筹失败，投资者手动执行 refund()，从合约帐户中转回之前参与投资的 ETH
+// 5，众筹状态机：
+//    PreFunding:   startsAt 之前
+//    Funding:      startsAt~endsAt 之间，raisedAmount<maxFundingGoalInWei
+//    Success:      startsAt~endsAt 之间：raisedAmount>=maxFundingGoalInWei，或者endsAt 之后，raisedAmount>=minFundingGoalInWei
+//    Failure:      endsAt 之后，raisedAmount<minFundingGoalInWei
 
 contract Crowdsale is SafeMath, ERC20 {
 
@@ -22,63 +35,52 @@ contract Crowdsale is SafeMath, ERC20 {
     enum State{PreFunding, Funding, Success, Failure}
     bool public finalizedCrowdfunding = false;
 
-    address public multisigWallet;
+    address public multiSigWallet;
 
-    event Invested(address indexed investor, uint256 weiAmount, uint256 tokenAmount);
+    // yangfeng: indexed 有什么作用？
+    event Invested(address indexed investor, uint256 weiAmount);
     event Refund(address indexed investor, uint256 weiAmount);
     //event EndsAtChanged(uint256 endsAt);
 
-    function Crowdsale(address _Multisig,
-                       uint256 _fundingStartBlock,
-                       uint256 _fundingEndBlock) {
-
-        if (_Multisig == 0) throw;
-        if (_fundingStartBlock <= block.number) throw;
-        if (_fundingEndBlock   <= _fundingStartBlock) throw;
+    function Crowdsale(
+        address _multiSigWallet,
+        uint256 _startsAt,
+        uint256 _endsAt
+    ) {
+        if (   _multiSigWallet == 0
+            || _startsAt <= block.timestamp
+            || _endsAt <= _startsAt) 
+            throw;
         isCrowdsale = true;
-        startsAt = _fundingStartBlock;
-        endsAt = _fundingEndBlock;
-        multisigWallet = _Multisig;
-        if (!MultiSigWallet(multisigWallet).isMultiSigWallet()) throw;
+        multiSigWallet = _multiSigWallet;
+        startsAt = _startsAt;
+        endsAt = _endsAt;
+        if (!MultiSigWallet(multiSigWallet).isMultiSigWallet()) throw;
     }
 
-    function setMultiSigWallet(address newWallet) external {
-      if (msg.sender != multisigWallet) throw;
-      MultiSigWallet wallet = MultiSigWallet(newWallet);
-      if (!wallet.isMultiSigWallet()) throw;
-      multisigWallet = newWallet;
+    function setMultiSigWallet(address newWallet) 
+        external 
+    {
+        if (msg.sender != multiSigWallet) throw;
+        MultiSigWallet wallet = MultiSigWallet(newWallet);
+        if (!wallet.isMultiSigWallet()) throw;
+        multiSigWallet = newWallet;
     }
 
-    function() { throw; }
-
-    function invest() payable external {
+    function () {
         if (getState() != State.Funding) throw;
 
-        receiver = msg.sender;
-
+        investor = msg.sender;
         uint256 weiAmount = msg.value;
-        //uint256 tokenAmount = calculatePrice(weiAmount, token.decimals());
-        //if(tokenAmount == 0) throw;
-        if(weiAmountOf[receiver] == 0) {
+        // update investorCount, weiAmountOf, weiRaised
+        if(weiAmountOf[investor] == 0) {
            investorCount++; // A new investor
         }
-
-        weiAmountOf[receiver] = weiAmountOf[receiver].plus(weiAmount);
-        //tokenAmountOf[receiver] = tokenAmountOf[receiver].plus(tokenAmount);
-        weiRaised = weiRaised.plus(weiAmount);
-        tokensSold = tokensSold.plus(tokenAmount);
-
-        if(isBreakingCap(weiAmount, tokenAmount, weiRaised, tokensSold)) throw;
-        //if(!token.transferFrom(beneficiary, receiver, tokenAmount)) throw;
-
-        // if(!multisigWallet.send(weiAmount)) throw;
-
-        Invested(receiver, weiAmount, tokenAmount, customerId);
-    }
-
-
-
-    function calculatePrice(uint256 weiAmount, uint256 decimals) {
+        weiAmountOf[investor] = safeAdd(weiAmountOf[investor], weiAmount);
+        weiRaised = safeAdd(weiRaised, weiAmount);
+        // yangfeng: 是否需要考虑最后一单 ETH 超过 maxFundingGoalInWei，截取剩余部分返还
+        if(weiRefunded > maxFundingGoalInWei) throw;
+        Invested(investor, weiAmount);
     }
 
     function finalizeCrowdfunding() external {
@@ -86,26 +88,26 @@ contract Crowdsale is SafeMath, ERC20 {
         if (finalizedCrowdfunding) throw; // can't finalize twice (so sneaky!)
 
         finalizedCrowdfunding = true;
-
-        if (!multisigWallet.send(this.balance)) throw;
+        if (!multiSigWallet.send(this.balance)) throw;
     }
 
     function refund() external {
         if (getState() != State.Failure) throw;
 
-        uint256 weiValue = weiAmountOf[msg.sender];
-        if (weiValue == 0) throw;
-        weiAmountOf[msg.sender] = 0;
-        weiRefunded = weiRefunded.plus(weiValue);
-        Refund(msg.sender, weiValue);
-        if (!msg.sender.send(weiValue)) throw;
+        address receiver = msg.sender;
+        uint256 weiAmount = weiAmountOf[receiver];
+        if (weiAmount == 0) throw;
+        weiAmountOf[receiver] = 0;
+        weiRefunded = safeAdd(weiRefunded, weiAmount);
+        Refund(receiver, weiAmount);
+        if (!receiver.send(weiAmount)) throw;
     }
 
     function getState() public constant returns (State){
       if (finalizedCrowdfunding) return State.Success;
-      if (block.number < startsAt) return State.PreFunding;
-      else if (block.number <= endsAt && totalSupply < tokenCreationMax) return State.Funding;
-      else if (totalSupply >= tokenCreationMin) return State.Success;
+      if (block.timestamp < startsAt) return State.PreFunding;
+      else if (block.timestamp <= endsAt && weiRaised < maxFundingGoalInWei) return State.Funding;
+      else if (weiRaised >= minFundingGoalInWei) return State.Success;
       else return State.Failure;
     }
 }
